d63f839a7761c0816f8c255f29123eb370580e9c
Antonio Decastro <36793302+ACDecastro@users.noreply.github.com>
2021-02-12 18:00:51 -0500

    Added modal with checkboxes. Set up empty functions

--- script.js
+++ script.js
@@ -3,11 +3,40 @@ var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+//   var symbolSelect;
+//   var pwLength = prompt("Please enter how many characters long you would like your password to be", "Please enter a number from 8 to 128");
+//   if (allnumeric(pwLength)){
 
-  passwordText.value = password;
-  console.log(password);
+//   }
+
+//   var password = generatePassword();
+//   var passwordText = document.querySelector("#password");
+
+//   passwordText.value = password;
+//   console.log(password);
+$('#exampleModal').modal('show');
+
+}
+
+function allnumeric(inputtxt) //This function taken from https://www.w3resource.com/javascript/form/all-numbers.php
+   {
+      var numbers = /^[0-9]+$/;
+      if(inputtxt.value.match(numbers))
+      {
+      //alert('Your Registration number has accepted....');
+      //document.form1.text1.focus();
+      return true;
+      }
+      else
+      {
+      //alert('Please input numeric characters only');
+      //document.form1.text1.focus();
+      return false;
+      }
+   } 
+
+function generatePassword(){
+
 }
 
 // Add event listener to generate button
