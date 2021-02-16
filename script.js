909cf289604e09ae2b75a64e76ec453ceab400a3
Antonio Decastro <36793302+ACDecastro@users.noreply.github.com>
2021-02-16 18:18:50 -0500

    Wrote some code to randomly assemble password from selected symbol types

--- script.js
+++ script.js
@@ -4,13 +4,14 @@ var lowercaseCheck = document.querySelector(".lower");
 var uppercaseCheck = document.querySelector(".upper");
 var digitCheck = document.querySelector(".digits");
 var specialCheck = document.querySelector(".specials");
+var saveButton = document.querySelector("#saveButton");
 
 //These strings contain all the necessary characters for passwords
 var lowercaseLetters = 'qwertyuiopasdfghjklzxcvbnm';
 var uppercaseLetters = 'QWERTYUIOPASDFGHJKLZXCVBNM';
 var specials = " !\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~";
 var digits = "0123456789";
-
+var passwordLength = 0;
 // Write password to the #password input
 function writePassword() {
 //   var symbolSelect;
@@ -19,6 +20,7 @@ function writePassword() {
    console.log("value of allnumeric: " + allnumeric(pwLength));
    if (allnumeric(pwLength)==true){
       console.log("all numbers");
+      passwordLength = parseInt(pwLength);
    }
    else{
       alert('Please input numeric characters only');
@@ -27,12 +29,6 @@ function writePassword() {
 
    //Have the user select at least one symbol type to include
    $('#symbolSelect').modal('show');
-
-//   var password = generatePassword();
-//   var passwordText = document.querySelector("#password");
-
-//   passwordText.value = password;
-//   console.log(password);
 }
 
 function allnumeric(inputtxt){ //This function taken from https://www.w3resource.com/javascript/form/all-numbers.php
@@ -47,12 +43,40 @@ function allnumeric(inputtxt){ //This function taken from https://www.w3resource
       }
 } 
 
-//Finally assemble password with all information
+//Finally assemble password with all information and write it to the password field
 function generatePassword(){
+   var newPassword = "";
+   for(var i = 0; i<=passwordLength; i++){
+      var type = Math.floor(Math.random()*4);
+      var symbolString = "";
+      switch(type){
+         case 0: 
+            symbolString = lowercaseLetters;
+            break;
+         case 1: 
+            symbolString = uppercaseLetters;
+            break;
+         case 2: 
+            symbolString = specials;
+            break;
+         case 3: 
+            symbolString = digits;
+            break;
+         default:
+            console.log("Something weird happened");
+            break;
+      }
+      var symbolChar = symbolString.charAt(Math.floor(Math.random()*(symbolString.length-1)));
+      newPassword = newPassword + symbolChar;
+   }
+   var passwordText = document.querySelector("#password");
 
+   passwordText.value = newPassword;
+   console.log(newPassword);
+   alert("Your new password is "+newPassword);
 }
 
-//When someone clicks a checkbox, that box' value gets multiplied by negative 1 to keep track of checked/unchecked status
+//When someone clicks a checkbox, that box's value gets multiplied by negative 1 to keep track of checked/unchecked status
 function selectLower(){
    document.getElementById("lowercaseCheckbox").value *= -1;
 }
@@ -74,3 +98,5 @@ lowercaseCheck.addEventListener("click", selectLower);
 uppercaseCheck.addEventListener("click", selectUpper);
 digitCheck.addEventListener("click", selectDigits);
 specialCheck.addEventListener("click", selectSpecials);
+
+saveButton.addEventListener("click", generatePassword);
